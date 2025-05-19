c9b67ae0e5a4bd261349175633674ac11f3b7aa4
Fandi Nurrokhim <fandinurrokhim56@Gmail.com>
2025-05-19 19:04:25 +0700

    chore: change footer

--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -7,9 +7,7 @@
                     <script>
                         document.write(new Date().getFullYear() + ",");
                     </script>
-                    made with <i class="ri-heart-fill text-red-500"></i> by
-                    <a href="https://github.com/Akbarwp" class="font-semibold text-gray-500 dark:text-purple-200" target="_blank">AkbarWP</a>
-                    for a better web.
+                    AHP SYSTEM INDAH
                 </div>
             </div>
         </div>
