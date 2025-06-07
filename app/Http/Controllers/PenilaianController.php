d5910d169029b542353db8a9acf77417c0456f99
Fandi Nurrokhim <fandinurrokhim56@Gmail.com>
2025-06-07 14:13:51 +0700

    chanbge PDF to Pdf

--- app/Http/Controllers/PenilaianController.php
+++ app/Http/Controllers/PenilaianController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
-use Barryvdh\DomPDF\Facade\PDF;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Services\KategoriService;
